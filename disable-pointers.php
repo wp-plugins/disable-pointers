ddf10011b8a6ed30880338a6b741104aba73a555
lumpysimon <lumpysimon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-17 12:44:58 +0000

    Initial commit
    git-svn-id: https://plugins.svn.wordpress.org/disable-pointers/trunk@491111 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ disable-pointers.php
@@ -0,0 +1,70 @@
+<?php
+/*
+Plugin Name: Disable Pointers
+Plugin URI: http://lumpylemon.co.uk/plugins/wordpress-disable-admin-pointers
+Description: Disable the admin pointer tooltips introduced in WordPress 3.3
+Version: 0.1
+Author: Simon Blackbourn @ Lumpy Lemon
+Author URI: http://lumpylemon.co.uk
+
+
+
+	This is a plugin for WordPress (http://wordpress.org).
+
+	Released under the GPL license:
+	http://www.opensource.org/licenses/gpl-license.php
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+
+
+*/
+
+
+
+class lumpyNoPointers {
+
+
+
+	public function __construct() {
+
+		add_action( 'wp_default_scripts', array( $this, 'no_pointer_script' ) );
+		add_action( 'wp_default_styles',  array( $this, 'no_pointer_style'  ) );
+
+	}
+
+
+
+	function no_pointer_script( $scripts ) {
+
+		$scripts->remove( 'wp-pointer' );
+
+	}
+
+
+
+	function no_pointer_style( $styles ) {
+
+		$styles->remove( 'wp-pointer' );
+
+	}
+
+
+
+}
+
+
+
+$lumpy_no_pointers = new lumpyNoPointers;
+
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== Disable Pointers ===
+Contributors: lumpysimon
+Donate link: http://lumpylemon.co.uk/donate
+Tags: admin, pointers, tooltips, disable
+Requires at least: 3.3
+Tested up to: 3.3.1
+Stable tag: trunk
+
+Disable the pointers (tooltips) introduced to admin screens in WordPress 3.3
+
+== Description ==
+
+When you upgrade to WordPress 3.3 (or later) from an earlier version, all users are shown various helpful pointers (tooltips) when they first log in to the admin screens. This simple plugin disables them for all users. No options, no configuration, just install and activate.
+
+== Frequently Asked Questions ==
+
+= Is that all there is? =
+
+Yes.
+
+== Installation ==
+
+1. Unzip disable-pointers.zip and upload the folder and its files to your wp-content/plugins directory.
+2. Activate the plugin through the Plugins menu in WordPress.
+
+== Changelog ==
+
+= 0.1 =
+* Initial release
